bd1cc8f6cf0c3252c7e594bb31e81e956cca46ef
ccfeel <51542473+ccfeel@users.noreply.github.com>
2020-09-01 13:11:39 +0800

    Update resource-parser.js

--- resource-parser.js
+++ resource-parser.js
@@ -69,7 +69,7 @@
 
 /**
 * 使用说明，
-0️⃣ 在QuantumultX 配置文件中[general] 部分，加入 resource_parser_url=https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/resource-parser.js
+0️⃣ 在QuantumultX 配置文件中[general] 部分，加入 resource_parser_url=https://github.com/ccfeel/aa/edit/master/resource-parser.js
 ⚠️⚠️如提示"没有自定义解析器"，请长按右下角图标后点击左侧刷新按钮，更新资源，后台退出 app，直到出现解析器说明
 1️⃣ 假设原始订阅连接为: https://raw.githubusercontent.com/crossutility/Quantumult-X/master/server-complete.txt , 
 2️⃣ 假设你想要保留的参数为 in=tls+ss, 想要过滤的参数为 out=http+2, 请注意下面订阅链接后一定要加 ”#“ 符号
